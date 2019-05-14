4db82d28205daa8480a05ef66c71cacd6845cf38
allycs <allycs@126.com>
2019-05-14 20:21:10 +0800

    修正页面时间初始化问题

---
+++ WangShunManager/js/init.search.date.js
@@ -0,0 +1,4 @@
+﻿var dateTime = new Date();
+var startTime = dateTime.Format("yyyy/MM/dd 00:00:00");
+dateTime.setDate(dateTime.getDate() + 1);
+var endTime = dateTime.Format("yyyy/MM/dd 00:00:00");
